7efc411bdd835f399eda847f30eb25877d9e732c
guidsantos <guilherme.fast2503@gmail.com>
2021-03-31 14:04:58 -0300

    CreateCategory Test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,42 @@
+import AppError from "../../../../errors/AppError"
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory"
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
+
+describe('Create Category', () => {
+
+    let createCategoryUseCase: CreateCategoryUseCase
+    let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+    })
+
+    it('should be able to create a new category', async () => {
+        const category = {
+            name: 'Category Test',
+            description: 'Category description Test'
+        }
+
+        await createCategoryUseCase.execute(category)
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name)
+
+
+        expect(categoryCreated).toHaveProperty('id')
+
+    })
+
+    it('should no be able to create a new category with same name', async () => {
+        expect(async () => {
+            const category = {
+                name: 'Category Test',
+                description: 'Category description Test'
+            }
+
+            await createCategoryUseCase.execute(category)
+            await createCategoryUseCase.execute(category)
+        }).rejects.toBeInstanceOf(AppError)
+
+    })
+})
